0ec311e6b8939c7a7303fdd1773de1622f7a111a
Dbolarinwa91 <dbolarinwa91@gmail.com>
2025-06-01 22:33:44 -0500

    updated the dynamodb table and lambda function

--- lambda.tf
+++ lambda.tf
@@ -7,46 +7,6 @@ resource "aws_dynamodb_table" "contact_submissions" {
     name = var.dynamodb_hash_key
     type = var.dynamodb_hash_key_type
   }
-  attribute {
-    name = "fullName"
-    type = var.dynamodb_fullName_type
-  }
-  attribute {
-    name = "email"
-    type = var.dynamodb_email_type
-  }
-  attribute {
-    name = "company"
-    type = var.dynamodb_company_type
-  }
-  attribute {
-    name = "phone"
-    type = var.dynamodb_phone_type
-  }
-  attribute {
-    name = "message"
-    type = var.dynamodb_message_type
-  }
-  attribute {
-    name = "submittedAt"
-    type = var.dynamodb_submittedAt_type
-  }
-  attribute {
-    name = "status"
-    type = var.dynamodb_status_type
-  }
-  attribute {
-    name = "ipAddress"
-    type = var.dynamodb_ipAddress_type
-  }
-  attribute {
-    name = "userAgent"
-    type = var.dynamodb_userAgent_type
-  }
-  attribute {
-    name = "source"
-    type = var.dynamodb_source_type
-  }
 }
 
 resource "aws_iam_role" "lambda_exec" {
