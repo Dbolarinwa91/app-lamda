d269e2bf77c531c13f1f33af0ddce3cfb4b5a253
Dbolarinwa91 <dbolarinwa91@gmail.com>
2025-06-01 21:24:39 -0500

    updated the lambda function name

--- lambda.tf
+++ lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "contact_submissions" {
-  name         = "ContactSubmissions"
+  name         = "contactsubmission-devops-david-site-project"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
 
@@ -9,7 +9,22 @@ resource "aws_dynamodb_table" "contact_submissions" {
   }
 }
 
+resource "aws_lambda_function" "contact_submission" {
+  function_name = "contactSubmissionHandler"
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
 
+  filename         = "lambda/contactSubmission.zip"
+  source_code_hash = filebase64sha256("lambda/contactSubmission.zip")
+
+  role = aws_iam_role.lambda_exec.arn
+
+  environment {
+    variables = {
+      CONTACTS_TABLE_NAME = aws_dynamodb_table.contact_submissions.name
+    }
+  }
+}
 
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
