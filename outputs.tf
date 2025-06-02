8e1e6744e7684c36198308809000bc3c84a1c06b
Dbolarinwa91 <dbolarinwa91@gmail.com>
2025-06-02 01:44:56 -0500

    added api gateway

--- outputs.tf
+++ outputs.tf
@@ -31,4 +31,19 @@ output "lambda_function_name" {
 output "lambda_function_arn" {
   description = "The ARN of the Lambda function"
   value       = aws_lambda_function.main.arn
-} 
+}
+
+output "api_gateway_rest_api_id" {
+  description = "The ID of the API Gateway REST API"
+  value       = aws_api_gateway_rest_api.lambda_api.id
+}
+
+output "api_gateway_stage_name" {
+  description = "The name of the API Gateway stage"
+  value       = aws_api_gateway_stage.prod.stage_name
+}
+
+output "api_invoke_url" {
+  description = "Invoke URL for the deployed API Gateway stage"
+  value       = "https://${aws_api_gateway_rest_api.lambda_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}/contact"
+}
