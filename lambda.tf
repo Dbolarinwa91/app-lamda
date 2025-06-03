27a00b0a20c5d9bbfcccb37e53282d208a5b0c21
Dbolarinwa91 <dbolarinwa91@gmail.com>
2025-06-03 00:10:41 -0500

    updated the lambda function

--- lambda.tf
+++ lambda.tf
@@ -9,6 +9,21 @@ resource "aws_dynamodb_table" "contact_submissions" {
   }
 }
 
+resource "aws_dynamodb_table" "newsletter_emails" {
+  name         = var.newsletter_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "newsletter-emails-table"
+  })
+}
+
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
