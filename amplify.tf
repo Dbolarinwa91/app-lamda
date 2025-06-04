0599be7aab5a1d5c04ba32e39d0cdbb2c291c7f7
Dbolarinwa91 <dbolarinwa91@gmail.com>
2025-06-04 08:36:58 -0500

    This changes allows the amplify app to be deployed automatically

--- amplify.tf
+++ amplify.tf
@@ -9,6 +9,29 @@ resource "aws_amplify_app" "pulse_robot" {
     VITE_CONTACT_API_URL       = "https://${aws_api_gateway_rest_api.lambda_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}/contact"
     VITE_NEWSLETTER_API_URL = "https://${aws_api_gateway_rest_api.lambda_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}/newsletter"
   }
+
+  # Add build settings
+  build_spec = <<-EOT
+    version: 1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - npm ci
+        build:
+          commands:
+            - npm run build
+      artifacts:
+        baseDirectory: dist
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
+
+  # Enable branch auto-deletion
+  enable_branch_auto_deletion = true
 }
 
 resource "aws_amplify_branch" "main" {
@@ -16,4 +39,12 @@ resource "aws_amplify_branch" "main" {
   branch_name = "main"
   stage       = "DEVELOPMENT"
   enable_auto_build = true
+
+  # Add build settings for the branch
+  framework = "React"  # or whatever framework you're using
+
+  # Add environment variables specific to this branch if needed
+  environment_variables = {
+    NODE_ENV = "development"
+  }
 } 
